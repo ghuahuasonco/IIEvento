7dc7b62ef8e5cf61d27c3e7efa543d988cce72fd
Gilberto Huahuasonco Huamantuma <ghuahuasonco@gmail.com>
2013-05-31 01:53:28 -0500

    evento v2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evento::Application.config.secret_token = '5ea1e5d0afff9eab8915d080bc75601bf3da5effe95d9a2f18fced1a9c211f6349ed8cb6703f6a61e08646e0d5d9e0ad090099f44f4332c2cedeb66913e010e8'
